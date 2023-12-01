2268030ac8647f237a019382f6dd26fab1c03510
YZeaFons <yeisonyamidzea@gmail.com>
2023-12-01 17:19:51 -0500

    Loading promises Hw

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,21 @@
 var http = require("http");
 var fs = require("fs");
-var data = require("./utils/data.js")
+// var data = require("./utils/data.js")
+const getCharById = require("./controllers/getCharById.js");
+
 
 const PORT = '3001'
 
 http.createServer((req, res) => {
-    console.log(`server creado en el puerto ${PORT}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     switch (true) {
         case req.url.includes(`/rickandmorty/character`):
             let urlArr = req.url.split('/')
             let id = parseInt(urlArr.pop())
-            let char = data.filter((char) => char.id === id)[0]
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(char))
-            // fs.readFile("./utils/data.js",(err,data) => {
-            //     if(err){
-
-            //     }
-            //  }
-            // )
+            // let char = data.filter((char) => char.id === id)[0]
+            // res.writeHead(200, { 'Content-Type': 'application/json' })
+            // res.end(JSON.stringify(char))
+            getCharById(res, id)
             return
 
         default:
@@ -28,4 +24,4 @@ http.createServer((req, res) => {
             return
 
     }
-}).listen(PORT, 'localhost')
+}).listen(PORT, '127.0.0.1')
